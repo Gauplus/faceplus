7813e3c49878f488c9ea903eaf148b354bfc3f86
吕进 <lvjindalaoban@163.com>
2018-12-26 21:44:41 +0800

    main_activity.xml底部的导航栏按钮加了点击效果

--- app/src/main/java/edu/njust/cn/faceplus/MainActivity.java
+++ app/src/main/java/edu/njust/cn/faceplus/MainActivity.java
@@ -1,8 +1,8 @@
 package edu.njust.cn.faceplus;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
