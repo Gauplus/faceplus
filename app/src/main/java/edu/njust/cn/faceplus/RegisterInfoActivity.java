ecc2db4c3ef0866b26c7d02549eeda1a3b847b9b
吕进 <lvjindalaoban@163.com>
2018-12-29 17:28:33 +0800

    改了几个图标

--- app/src/main/java/edu/njust/cn/faceplus/RegisterInfoActivity.java
+++ app/src/main/java/edu/njust/cn/faceplus/RegisterInfoActivity.java
@@ -1,26 +1,20 @@
 package edu.njust.cn.faceplus;
 
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,8 +25,6 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class RegisterInfoActivity extends AppCompatActivity {
     public static final int Take_photo = 1;
